aeffa4170481986974475e0ca1c5bd5e64e9c4e7
Junita Berglin <113436666+JunitaBerglin@users.noreply.github.com>
2023-03-15 13:17:08 +0100

    trying

--- seedDb/seedDb.js
+++ seedDb/seedDb.js
@@ -4,18 +4,25 @@ const mongoose = require('mongoose')
 const springTerm2023MockData = require('./mockdata/springTerm2023.json')
 // @ts-ignore
 const fallTermMock2023Data = require('./mockdata/fallTerm2023.json')
+const Dance = require('../src/models/Dance')
+const Participant = require('../src/models/Participant.js')
 
-const seedPresidentsDb = async (connectionString) => {
+const populateDbWithMockData = async (connectionString) => {
 	let conn
 	try {
 		mongoose.set('strictQuery', false)
-		conn = await mongoose.connect(connectionString)
+		conn = await mongoose.connect(
+			'mongodb+srv://JunitaBerglin:losenord1@cluster0.uuzml7c.mongodb.net/?retryWrites=true&w=majority'
+		)
+
+		console.log(`MongoDB connected: ${conn.connection.host}`)
 
 		// POPULATE DATA ACCOORDING TO YOUR MODELS
-		console.log(springTerm2023MockData)
-		console.log(fallTermMock2023Data)
+		await Dance.deleteMany()
+		await Dance.create(springTerm2023MockData)
+		await Dance.create(fallTermMock2023Data)
 
-		console.log('@TODO: SeedDb')
+		console.log('Database successfully populated with test data')
 	} catch (error) {
 		console.error(error)
 	} finally {
@@ -24,4 +31,4 @@ const seedPresidentsDb = async (connectionString) => {
 	}
 }
 
-seedPresidentsDb(process.env.MONGO_URI)
+populateDbWithMockData(process.env.MONGO_URI)
